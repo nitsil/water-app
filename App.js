73b1e0ff0335548962985725e6ebf4a508852c39
nitsil <nitsil@users.noreply.github.com>
2021-03-31 13:54:19 +0200

    update navigation

--- App.js
+++ App.js
@@ -3,22 +3,44 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-function HomeScreen() {
+function LoginScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      
+      <Text>Login Screen</Text>
+      <Button
+        title="Log in and continue"
+        onPress={() => navigation.navigate('WaterDetails')}
+      />
     </View>
   );
 }
+
+function DetailsScreen({ navigation }) {
+  return (
+    <>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Water Details Screen</Text>
+    </View>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>History</Text>
+  </View>
+
+  <Button title="Log out" onPress={() => navigation.popToTop()} />
+  </>
+  );
+}
+
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator initialRouteName="Login" >
+        <Stack.Screen name="Login" component={LoginScreen} options={{ title: 'Log in' }} />
+        <Stack.Screen name="WaterDetails" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
